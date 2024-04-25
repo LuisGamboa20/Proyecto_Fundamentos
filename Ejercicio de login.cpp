df732dd483d12489704370e7a00b4782cda4eea7
LuisGamboa20 <luiscarlosjunior2016@hotmail.com>
2024-04-25 18:31:23 -0500

    Se agrega el proyecto

---
+++ Ejercicio de login.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+
+using namespace std;
+
+int main(){
+	
+	setlocale(LC_CTYPE,"spanish");
+	
+	string nombre="Admin";
+	string password, password2;
+	cout<<"Ingrese el nombre de cuenta a crear\n";
+	getline(cin,nombre);
+	do{
+		cout<<"Ingrese una contraseña debe ser mayor o igual a 8 caracteres: \n";
+		cin>>password;
+		system("cls");
+		if(password.length() < 8 ){
+			cout<<"La contraseña no cumple los requisitos.\n";
+		}
+	}while(password.length() < 8);
+
+	do{
+		cout<<"Ingrese nuevamente la contraseña: \n";
+		cin>>password2;
+		system("cls");
+		if(password != password2){
+			cout<<"Contraseña invalida\n";
+		}
+	}while(password != password2);
+	
+	cout<<"Bienvenido a su cuenta Sr(a) "<<nombre;
+		
+	return 0;
+}
+
