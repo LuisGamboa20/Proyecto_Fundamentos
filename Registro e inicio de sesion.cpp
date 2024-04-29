e5f88c49b4e1134aefd17a4af794fe62af195698
LuisGamboa20 <luiscarlosjunior2016@hotmail.com>
2024-04-29 10:17:59 -0500

    Se agrega el switch para facilitar el ingreso de datos en el inicio

--- Registro e inicio de sesion.cpp
+++ Registro e inicio de sesion.cpp
@@ -6,33 +6,49 @@ using namespace std;
 
 int main(){
 	
-	//Registro//
-	
 	setlocale(LC_CTYPE,"spanish");
-	
-	string nombre="Admin";
-	string password, password2;
-	cout<<"Ingrese el nombre de cuenta a crear\n";
-	getline(cin,nombre);
-	do{
-		cout<<"Ingrese una contraseña debe ser mayor o igual a 8 caracteres: \n";
-		cin>>password;
-		system("cls");
-		if(password.length() < 8 ){
-			cout<<"La contraseña no cumple los requisitos.\n";
-		}
-	}while(password.length() < 8);
 
-	do{
-		cout<<"Ingrese nuevamente la contraseña: \n";
-		cin>>password2;
-		system("cls");
-		if(password != password2){
-			cout<<"Contraseña invalida\n";
-		}
-	}while(password != password2);
 	
-	cout<<"Bienvenido a su cuenta Sr(a) "<<nombre;
+	//Inicio//
+	string nombre="Admin",nombre2="Admin",password,password2;
+	int men_registro=0;
+	
+	cout<<"1)Registro e inicio de sesión\n";
+		cin>>men_registro;
+	system("cls");
+	switch(men_registro){
+		case 1:{
+			//Registro//
+			cout<<"Por favor ingrese un nombre para la cuenta: ";
+			cin>>nombre;
+			do{
+				cout<<"Por favor digite una contraseña mayor o igual a 8 caracteres: ";
+				cin>>password;
+				system("cls");
+				if(password.length() < 8){
+					cout<<"La contraseña no cumple los requisitos intentelo de nuevo\n";
+				}
+			}while(password.length() < 8);
+			//Inicio de sesión//
+			do{
+				cout<<"Por favor digite su nombre de usuario: ";
+				cin>>nombre2;
+				cout<<"Por favor digite su contraseña: ";
+				cin>>password2;
+				system("cls");
+				if(nombre2 == nombre && password2 == password){
+					cout<<"Bienvenido a su cuenta Sr(a) "<<nombre;
+				}else{
+					cout<<"Los datos no coinciden intentelo de nuevo\n";
+				}
+			}while(!(nombre2 == nombre && password2 == password));
+			break;
+		}
+		default:{
+			cout<<"Digite una opción valida.";
+			break;
+		}
+	}
 		
 	return 0;
 }
